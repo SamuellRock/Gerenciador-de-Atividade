73cd6d0d73705e3869a0227be4142a897fa39196
alpacah <pedronit2016@gmail.com>
2024-05-22 21:03:47 -0300

    Aquivo base02.html

--- cadastro/urls.py
+++ cadastro/urls.py
@@ -7,9 +7,11 @@ from .views import *
 urlpatterns = [
 
     path('cadastro_externo/', cadastro_externo, name='cdE'),
+    path('atualizar_externo/<int:id>/', atualizar_externo, name='atE'),
     path('cadastro_atividade/', cadastro_atividade, name='cdA'),
     path('inscricao/', inscricao, name='insc'),
     path('menu_atividade/', menu_atividade, name='menuAt'),
     path('lista_precenca/<int:atividade_id>/', lista_presenca, name='listP'),
+    path('lista/', lista_clientes, name='lt'),
 
 ]
